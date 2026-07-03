ee313e29583542dddba792716ef85b4ae6397107
Ben Lowery <ben.lowery1@tesco.com>
2026-07-03 15:36:40 +0100

    Fix bash

--- storm/test.sh
+++ storm/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J STORMing_the_Bayestille
 #SBATCH -c 10
-#SBATCH -o ./output_scripts/real_data_output.out
+#SBATCH -o ./output_scripts/%A_real_data_output.out
 
 time srun uv run ./storm/storm_test.py 9
 
